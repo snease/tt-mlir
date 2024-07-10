7a556c3f926ffe81575ddf53f9edfecdcfbbe83f
Milan Topalovic <mtopalovic@tenstorrent.com>
2024-07-10 10:29:58 +0000

    Add env variable to support runtime building

--- install.sh
+++ install.sh
@@ -7,5 +7,10 @@ build_type=${BUILD_TYPE:-Release}
 c_compiler=${C_COMPILER:-clang}
 cxx_compiler=${CXX_COMPILER:-clang++}
 
-cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler
+if [[ -n $TTMLIR_ENABLE_RUNTIME ]]; then
+    cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DTTMLIR_ENABLE_RUNTIME=ON
+else
+    cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+fi
+
 cmake --build build
