8bb7af5572937d22e7a648ed874d385fb826a228
Jacob DeSousa <jdesousa@tenstorrent.com>
2024-11-19 21:20:42 +0000

    Add get_noc_adr and get_read/write_ptr ops to ttkernel

--- lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
+++ lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
@@ -421,6 +421,8 @@ public:
                TTMetalToEmitCOpaqueRewriter<ttkernel::MulTilesOp>,
                TTMetalToEmitCOpaqueRewriter<ttkernel::ReduceInitOp>,
                TTMetalToEmitCOpaqueRewriter<ttkernel::ReduceTileOp>,
+               TTMetalToEmitCOpaqueRewriter<ttkernel::GetReadPtrOp>,
+               TTMetalToEmitCOpaqueRewriter<ttkernel::GetWritePtrOp>,
                TTMetalToEmitCOpaqueRewriter<ttkernel::GetNocAddrOp>,
                TTMetalToEmitCOpaqueRewriter<ttkernel::NocAsyncReadOp>,
                TTMetalToEmitCOpaqueRewriter<
